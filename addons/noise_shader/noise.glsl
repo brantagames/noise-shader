6b1fc6773a56697639eb2dea7f217eefdaf07204
Jack <jack@branta.games>
2024-08-29 19:55:21 -0400

    initial commit

---
+++ addons/noise_shader/noise.glsl
@@ -0,0 +1,58 @@
+#[compute]
+#version 450
+
+#define STEPS 3
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+layout(rgba16f, set = 0, binding = 0) uniform image2D screen_image;
+
+layout(push_constant, std430) uniform Params {
+    vec2 raster_size;
+    vec2 reserved;
+    int steps;
+} params;
+
+layout(set = 1, binding = 1, std430) restrict buffer FloatBufferIn {
+  float data[];
+}
+buffer_in;
+
+layout(set = 1, binding = 2, std430) restrict buffer FloatBufferOut {
+  float data[];
+}
+buffer_out;
+
+int get_buffer_index(in ivec2 uv, in ivec2 size) {
+    return uv.y * size.x + uv.x;
+}
+
+void update_value(inout float value, in vec4 screen_color) {
+    value = value + screen_color.r * 0.1;
+    value -= floor(value);
+}
+
+float get_brightness_from_value(in float value) {
+    float brightness = floor(value * 2.0 * float(params.steps - 1));
+    brightness = float(params.steps - 1) - abs(brightness - float(params.steps - 1));
+    return brightness / float(params.steps - 1);
+}
+
+void main() {
+    ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 size = ivec2(params.raster_size);
+
+    if (uv.x >= size.x || uv.y >= size.y) {
+        return;
+    }
+
+    int index = get_buffer_index(uv, size);
+    float value = buffer_in.data[index];
+    vec4 screen_color = imageLoad(screen_image, uv);
+
+    update_value(value, screen_color);
+    buffer_out.data[index] = value;
+
+    screen_color.rgb = vec3(pow(get_brightness_from_value(value), 3.0));
+    imageStore(screen_image, uv, screen_color);
+}
---
+++ addons/noise_shader/post_process_noise.gd
@@ -0,0 +1,145 @@
+extends CompositorEffect
+class_name PostProcessNoise
+
+
+const NOISE_SHADER_PATH: String = "res://addons/noise_shader/noise.glsl"
+const BYTES_PER_BUFFER_FLOAT: int = 4
+
+@export var steps: int = 3:
+	set(value):
+		steps = maxi(value, 2)
+
+var _huh: int = 0
+
+var _rendering_device: RenderingDevice
+var _shader: RID
+var _pipeline: RID
+
+var _buffer_size: int = 512 * 512 * BYTES_PER_BUFFER_FLOAT
+
+var _read_buffer: RID
+var _write_buffer: RID
+
+var _read_data: PackedByteArray = []
+var _write_data: PackedByteArray = []
+
+var _storage_set: RID
+
+
+func _init() -> void:
+	effect_callback_type = EFFECT_CALLBACK_TYPE_POST_TRANSPARENT
+	_rendering_device = RenderingServer.get_rendering_device()
+	RenderingServer.call_on_render_thread(_initialize_compute)
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PREDELETE:
+		if _shader.is_valid():
+			RenderingServer.free_rid(_shader)
+
+
+func _initialize_compute() -> void:
+	_rendering_device = RenderingServer.get_rendering_device()
+	if not _rendering_device:
+		return
+	
+	var shader_file: RDShaderFile = load(NOISE_SHADER_PATH)
+	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
+	
+	_shader = _rendering_device.shader_create_from_spirv(shader_spirv)
+	if not _shader.is_valid():
+		return
+	
+	_update_storage_set()
+	
+	_pipeline = _rendering_device.compute_pipeline_create(_shader)
+
+
+func _render_callback(effect_callback_type: int, render_data: RenderData) -> void:
+	if (
+			not _rendering_device
+			or effect_callback_type != EFFECT_CALLBACK_TYPE_POST_TRANSPARENT
+			or not _pipeline.is_valid()
+	):
+		return
+	
+	var render_scene_buffers: RenderSceneBuffersRD = render_data.get_render_scene_buffers()
+	if not render_scene_buffers:
+		return
+	
+	var size: Vector2i = render_scene_buffers.get_internal_size()
+	if size.x == 0 and size.y == 0:
+		return
+	
+	if not _buffer_size == size.x * size.y * BYTES_PER_BUFFER_FLOAT:
+		_buffer_size = size.x * size.y * BYTES_PER_BUFFER_FLOAT
+		_update_storage_set()
+	
+	@warning_ignore("integer_division")
+	var x_groups: int = (size.x - 1) / 8 + 1
+	@warning_ignore("integer_division")
+	var y_groups: int = (size.y - 1) / 8 + 1
+	var z_groups: int = 1
+	
+	var push_constant: PackedByteArray
+	push_constant.append_array(PackedFloat32Array([
+		size.x,
+		size.y,
+		0.0,
+		0.0,
+	]).to_byte_array())
+	push_constant.append_array(PackedInt32Array([steps]).to_byte_array())
+	push_constant.resize(32)
+	
+	_rendering_device.buffer_update(_read_buffer, 0, _buffer_size, _read_data)
+	
+	var view_count: int = render_scene_buffers.get_view_count()
+	for view_index: int in view_count:
+		var screen_image := RDUniform.new()
+		screen_image.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+		screen_image.binding = 0
+		var input_image: RID = render_scene_buffers.get_color_layer(view_index)
+		screen_image.add_id(input_image)
+		
+		var screen_set: RID = UniformSetCacheRD.get_cache(
+			_shader,
+			0, 
+			[screen_image],
+		)
+		
+		var compute_list: int = _rendering_device.compute_list_begin()
+		_rendering_device.compute_list_bind_compute_pipeline(compute_list, _pipeline)
+		_rendering_device.compute_list_bind_uniform_set(compute_list, screen_set, 0)
+		_rendering_device.compute_list_bind_uniform_set(compute_list, _storage_set, 1)
+		_rendering_device.compute_list_set_push_constant(
+			compute_list,
+			push_constant,
+			push_constant.size(),
+		)
+		_rendering_device.compute_list_dispatch(compute_list, x_groups, y_groups, z_groups)
+		_rendering_device.compute_list_end()
+	
+	_read_data = _rendering_device.buffer_get_data(_write_buffer)
+
+
+func _update_storage_set() -> void:
+	_read_data.resize(_buffer_size)
+	_write_data.resize(_buffer_size)
+	
+	var buffer_in := RDUniform.new()
+	buffer_in.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	buffer_in.binding = 1
+	_read_buffer = _rendering_device.storage_buffer_create(_buffer_size, _read_data)
+	buffer_in.add_id(_read_buffer)
+	
+	var buffer_out := RDUniform.new()
+	buffer_out.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	buffer_out.binding = 2
+	_write_buffer = _rendering_device.storage_buffer_create(_buffer_size, _write_data)
+	buffer_out.add_id(_write_buffer)
+	
+	_storage_set = _rendering_device.uniform_set_create(
+		[buffer_in, buffer_out],
+		_shader,
+		1,
+	)
