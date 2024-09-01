8f5ebba1e8f87af8c8e9fbb7d55b09cc807755e3
Jack <jack@branta.games>
2024-08-31 23:18:17 -0400

    add comments to noise shader

--- addons/noise_shader/noise.glsl
+++ addons/noise_shader/noise.glsl
@@ -1,12 +1,11 @@
 #[compute]
 #version 450
 
-#define STEPS 3
-
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 layout(rgba16f, set = 0, binding = 0) uniform image2D screen_image;
 
+// Details parameters to be sent to the shader
 layout(push_constant, std430) uniform Params {
     vec2 raster_size;
     vec2 reserved;
@@ -14,26 +13,31 @@ layout(push_constant, std430) uniform Params {
     float speed;
 } params;
 
+// The noise data the shader reads from
 layout(set = 1, binding = 1, std430) restrict buffer FloatBufferIn {
-  float data[];
+    float data[];
 }
 buffer_in;
 
+// The noise data that the shader writes to
 layout(set = 1, binding = 2, std430) restrict buffer FloatBufferOut {
-  float data[];
+    float data[];
 }
 buffer_out;
 
+// Returns the index of the buffer according to the uv coordinate
 int get_buffer_index(in ivec2 uv, in ivec2 size) {
     return uv.y * size.x + uv.x;
 }
 
+// Update the buffer value according to the screen color and speed
 void update_value(inout float value, in vec4 screen_color) {
     float average = (screen_color.r + screen_color.g + screen_color.b) / 3.0;
     value = value + clamp(average, 0.0, 1.0) * params.speed;
     value -= floor(value);
 }
 
+// Returns a brightness value according to the buffer value and number of steps
 float get_brightness_from_value(in float value) {
     float brightness = floor(value * 2.0 * float(params.steps - 1));
     brightness = float(params.steps - 1) - abs(brightness - float(params.steps - 1));
@@ -55,6 +59,7 @@ void main() {
     update_value(value, screen_color);
     buffer_out.data[index] = value;
 
+    // Here the output pixel color is set with a cubic function to make the colors more even.
     screen_color.rgb = vec3(pow(get_brightness_from_value(value), 3.0));
     imageStore(screen_image, uv, screen_color);
 }
